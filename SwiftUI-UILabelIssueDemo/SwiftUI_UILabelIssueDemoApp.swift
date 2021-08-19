7cdfca3fb941f84b8000566c6bd4b6405ab09c7a
史 翔新 <x_shi@yumemi.co.jp>
2021-08-19 16:42:36 +0900

    Init Project

---
+++ SwiftUI-UILabelIssueDemo/SwiftUI_UILabelIssueDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_UILabelIssueDemoApp.swift
+//  SwiftUI-UILabelIssueDemo
+//
+//  Created by 史 翔新 on 2021/08/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_UILabelIssueDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
